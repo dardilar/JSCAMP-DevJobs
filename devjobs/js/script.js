248fa8d45c9441fed8bbdc2631502eafe012ebc5
dsardila <dsardila1809@hotmail.com>
2025-10-14 14:10:23 -0500

    [REFACTOR] Simplify apply button click handler by removing toggle functionality

--- devjobs/js/script.js
+++ devjobs/js/script.js
@@ -5,18 +5,11 @@ const searchResults = document.querySelector('.search__results');
 // Apply button change styles
 searchResults.addEventListener('click', function(e) {
     const applyBtn = e.target.closest('.apply__btn');
-    const applyBtnActive = e.target.closest('.apply__btn--active');
 
     if(applyBtn) {
         e.preventDefault();
         applyBtn.textContent = '¡Aplicado!';
         applyBtn.classList.add('apply__btn--active');
-    }
-
-    if(applyBtnActive) {
-        e.preventDefault();
-        applyBtnActive.classList.remove('apply__btn--active');
-        applyBtnActive.textContent = 'Aplicar';
     }
 })
 
