d9f0a7346a8cdc977b7497fcd489dc0afdc53628
dsardila <dsardila1809@hotmail.com>
2025-10-15 22:18:57 -0500

    [ADD] Custom avatar web component with unavatar.io integration

---
+++ devjobs/js/devjobs-avatar-element.js
@@ -0,0 +1,38 @@
+'use strict'
+
+class DevJobsAvatar extends HTMLElement {
+    constructor() {
+        super(); // Llama al constructor de la clase padre HTMLElement
+
+        this.attachShadow({ mode: 'open' });
+    }
+
+    createUrl(service, username) {
+        return `https://unavatar.io/${service}/${username}`;
+    }
+
+    render() {
+        const service = this.getAttribute('service') ?? 'github';
+        const username = this.getAttribute('username') ?? 'dardilar';
+        const size = this.getAttribute('size') ?? '40';
+
+        const url = this.createUrl(service, username);
+        
+        this.shadowRoot.innerHTML = `
+            <style>
+                .avatar {
+                    border-radius: 50%;
+                }
+            </style>
+
+            <img src="${url}" alt="Imagen de perfil" class="avatar" width="${size}" height="${size}">`;
+    }
+
+    //Cuando el elemento se inserta en el DOM
+    connectedCallback() {
+        this.render();
+    }
+}
+
+// Registra el elemento personalizado
+customElements.define('devjobs-avatar', DevJobsAvatar);
