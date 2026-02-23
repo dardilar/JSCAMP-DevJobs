84972b2b54458effcb1f9eed7373625d15856304
dsardila <dsardila1809@hotmail.com>
2026-02-23 17:52:52 -0500

    [ADD] Implement favorites context and reorder provider hierarchy
    - Added FavoritesProvider import from context/FavContext
    - Wrapped App with FavoritesProvider inside AuthProvider
    - Moved BrowserRouter to outermost provider position
    - Changed quote style from single to double quotes throughout file

--- react-devjobs/src/main.jsx
+++ react-devjobs/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { AuthProvider } from './context/AuthContext'
-import { BrowserRouter } from 'react-router'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { AuthProvider } from "./context/AuthContext";
+import { FavoritesProvider } from "./context/FavContext";
+import { BrowserRouter } from "react-router";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
